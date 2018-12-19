0a5903ded479f732bb96a856c1361558ca0e3825
YashSaxena75 <yashsaxena986@gmail.com>
2018-12-20 01:05:47 +0530

    check this out my first python program

---
+++ concat.py
@@ -0,0 +1,4 @@
+#concat
+print("He"+"y"+" this is go"+"ing to be a ve"+"ry Weird code"+" Bcz this is writ"+"ten with +")
+input("Do you want to print hello 5 times if yes then just press enter")
+print("\nHello"*5)
