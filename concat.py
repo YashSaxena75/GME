fd40eb845c3fd5e507c518808daf3838aedc6d85
YashSaxena75 <yashsaxena986@gmail.com>
2018-12-20 01:22:51 +0530

    some pythonic maths

--- concat.py
+++ concat.py
@@ -1,4 +1,4 @@
-#concat
+#concat # use of '//'
 print("He"+"y"+" this is go"+"ing to be a ve"+"ry Weird code"+" Bcz this is writ"+"ten with +"+"\nany ways good bye")
 print()
 print()
@@ -7,3 +7,9 @@ print()
 print("\nSo u are still here huh!!!ok fine let us do some work then")
 input("Do you want to print hello 5 times if yes then just press enter")
 print("\nHello"*5)
+print("Let us do some pythonic maths")
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+print(2//3)
