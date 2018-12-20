e7b13765a45c3a6fd4cbf9a06fbc1215d3732297
YashSaxena75 <yashsaxena986@gmail.com>
2018-12-20 15:23:04 +0530

    String operations

---
+++ strings.py
@@ -0,0 +1,10 @@
+#Use of different function that can be applied on the strings
+#let us take a string to do some operations
+quote="You are mine and I am proud of you my girl"
+print("Your original qoute is","\\",quote,"\\")
+print("Quotes in upper case","\\",quote.upper(),"\\")
+print("Quote in lower case","\\",quote.lower(),"\\")
+print("Quote title","\\",quote.title(),"\\")
+print("Replacing girl with lady","\\",quote.replace("girl","lady"),"\\")
+print("\a")
+print("Don't worry we have your original quote","\\",quote,"\\")
