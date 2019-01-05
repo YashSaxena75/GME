1268df191158edd131e29d22e68af2556d959679
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-01-05 16:21:24 +0530

    Create new game.py

---
+++ new game.py
@@ -0,0 +1,24 @@
+print("Welcome to the new game")
+print("Hey let us check you got something or not")
+bag=()
+if not bag:
+ print("Bad luck!!! u r empty")
+print("But don't worry dragon has left some of his things let me fill those in ur bag")
+bag=("Poison","Battery","SIM card","SD card","5G net pack")
+print("You have got:")
+for x in bag:
+ print(x)
+print("Are Chu@iy# mobile phone kaha hai!!!!")
+if "Poison" in bag:
+ print("Poison hai Daaru smjh ke pee le")
+print("Waah!!! main zindaa hu abhi bhi poison peeke")
+print("I have got something under the old banyan tree let me add those thimgs in ur bag")
+bags=("Mobile phone","Internet connection","Jio Sim","5G Tower")
+bag+=bags
+for h in bag:
+ print(h)
+print("Ab dekhunga mirzapur net on karke bc queen jaye bhaad me dragon jaye bhaad me")
+print("thaye thaye thaye PUBG PUBG PUBG PUBG")
+print("chacha","Oooooo bhosdiw@#$ chacha zara music bnd karlo aur thoda rest karlo warna rest in peace ho jaoge")
+print("important ye nahi ki munna chuti#^ hai important ye hai ki iss bar munna galat nahi tha")
+input("Press Enter to exit")
