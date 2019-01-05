9c3d355b191c14ca54b44f01d7dfd6d885674ca4
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-01-05 17:55:11 +0530

    something has removed

--- new game.py
+++ new game.py
@@ -8,7 +8,7 @@ bag=("Poison","Battery","SIM card","SD card","5G net pack")
 print("You have got:")
 for x in bag:
  print(x)
-print("Are Chu@iy# mobile phone kaha hai!!!!")
+print("Are mobile phone kaha hai!!!!")
 if "Poison" in bag:
  print("Poison hai Daaru smjh ke pee le")
 print("Waah!!! main zindaa hu abhi bhi poison peeke")
@@ -17,8 +17,8 @@ bags=("Mobile phone","Internet connection","Jio Sim","5G Tower")
 bag+=bags
 for h in bag:
  print(h)
-print("Ab dekhunga mirzapur net on karke bc queen jaye bhaad me dragon jaye bhaad me")
+print("Ab dekhunga mirzapur net on karke,queen jaye bhaad me dragon jaye bhaad me")
 print("thaye thaye thaye PUBG PUBG PUBG PUBG")
-print("chacha","Oooooo bhosdiw@#$ chacha zara music bnd karlo aur thoda rest karlo warna rest in peace ho jaoge")
-print("important ye nahi ki munna chuti#^ hai important ye hai ki iss bar munna galat nahi tha")
+print("chacha","Oooooo chacha zara music bnd karlo aur thoda rest karlo warna rest in peace ho jaoge")
+print("important ye nahi ki munna ------ hai important ye hai ki iss bar munna galat nahi tha")
 input("Press Enter to exit")
