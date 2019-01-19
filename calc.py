24dc20366a9ca0c6ae59b39418d94775ea3f7994
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-01-19 14:24:14 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@ import re
 print("Welcome to my magical calculator")
 print("--------------------Instructions-------------------")
 print("""
-                              1.Type '\q' to exit anytime    
+                              1.Type '\q' anytime to exit
                               2.ur equation must look like(a+b) 
                               3.you can do as many calculations as u want                         """)
 run=True
