9e2ae041788545b196936a6c72788a20aed15da9
Yash Saxena <35332582+YashSaxena75@users.noreply.github.com>
2019-01-19 16:36:54 +0530

    added reset feature and sleep function

--- calc.py
+++ calc.py
@@ -1,10 +1,13 @@
 import re
+import time
+import os
 print("Welcome to my magical calculator")
 print("--------------------Instructions-------------------")
 print("""
                               1.Type '\q' anytime to exit    
                               2.ur equation must look like(a+b) 
-                              3.you can do as many calculations as u want                         """)
+                              3.you can do as many calculations as u want   
+                              4.Type R to reset the calculator                            """)
 run=True
 prev=0
 def hell():
@@ -14,9 +17,13 @@ def hell():
      if prev==0:
       eq=input("Enter Your equation here:")
      else:
-          print(">>>",end=" ")
+          print("Enter more numbers to perfrom more operartions or \q to exit:",end=" ")
           eq=input(str(prev))
           print(" ")
+     if eq=='R':
+          print("Reseting----->>>")
+          time.sleep(2)
+          print("\n"*35)
      if eq == '\q':
           print("Good-Bye")
           print("Made by humanoid_hater(Yash Saxena)")
@@ -33,7 +40,8 @@ def hell():
             try:
              prev=eval(str(prev)+eq)
             except:
-             print("I can't think upto infinity....give another try")
+             print("I can't think upto infinity....giver another try")
+
 
 while run:
      hell()
